f018759922175a8cf9af36ae3d448a9587986a02
nowallcloud9_004 <nowallcloud9+004@gmail.com>
2016-10-22 01:59:50 +0000

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create(
+  name: "JavaScript入門",
+  price: 2700,
+  publish: "A社",
+  publish_date: "2016-08-01"
+)
+
+Book.create(
+  name: "Ruby入門",
+  price: 2000,
+  publish: "B社",
+  publish_date: "2016-09-01"
+)
+
+book3 = Book.new(
+  name: "Ruby on Rails入門",
+  price: 3000,
+  publish: "C社",
+  publish_date: "2016-10-01"
+)
+
+book3.save
